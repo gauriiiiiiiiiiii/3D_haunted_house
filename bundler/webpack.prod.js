0b324a65ee868eab2d356cf827cb6c52d21f04f1
Gauri <gauriagarwal25@gmail.com>
2025-06-17 20:51:04 +0530

    Fix: Added index.html and updated production webpack config

--- bundler/webpack.prod.js
+++ bundler/webpack.prod.js
@@ -1,14 +1,23 @@
-const webpackMerge = require('webpack-merge')
-const commonConfiguration = require('./webpack.common.js')
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const webpackMerge = require('webpack-merge');
+const commonConfiguration = require('./webpack.common.js');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin'); // 👈 Add this
 
 module.exports = webpackMerge(
-    commonConfiguration,
-    {
-        mode: 'production',
-        plugins:
-        [
-            new CleanWebpackPlugin()
-        ]
-    }
-)
+  commonConfiguration,
+  {
+    mode: 'production',
+    plugins: [
+      new CleanWebpackPlugin(),
+      new HtmlWebpackPlugin({
+        template: './src/index.html', // 👈 Make sure this file exists!
+        minify: {
+          collapseWhitespace: true,
+          removeComments: true,
+          removeRedundantAttributes: true,
+          useShortDoctype: true,
+        }
+      })
+    ]
+  }
+);
